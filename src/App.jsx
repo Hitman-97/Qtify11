9499bf57b3c88b6de030c38e540ecdb24d9228a5
Hitman-97 <150859674+Hitman-97@users.noreply.github.com>
2025-08-26 22:35:55 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+// import "./App.css";
+import Card from "./components/Card/Card";
+//@ts-ignore
+import NavBar from "./components/NavBar/NavBar";
+import { fetchTopAlbums } from "./components/api/api";
+import Section from "./components/Sections/Section";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App">
+      <NavBar />
+      {/* <HeroSection />
+      <div className="sectionWrapper" >
+      <Section type="album" title="Top Albums" data={topAlbumData} />
+      </div> */}
+      {/* <div className="cardContainer">
+      {topAlbumData.map((item) => {
+        return <Card key={item.id} data={item} type="album" />;
+      })}
+      </div> */}
+    </div>
+  );
 }
 
-export default App
+export default App;
